639d4c82efef51c0551529bf853eab235fe96c1d
Martin Burke <Martin.Burke@Gamesys.co.uk>
2018-01-30 10:31:44 +0000

    first run

--- src/emulator.js
+++ src/emulator.js
@@ -15,11 +15,46 @@ function placeNextBet ()
 {
     //TODO - go through all games on the current date and find the most suitable bet
     //if none exist, move on to the next day until one is found. 
+    
+    var thisDate = this._data [this._currentIndex].date;
+    var nextDate = this._data [++this._currentIndex].date;
+    var bestBet = null;
 
-    for ( ; this._currentIndex < this._data.length-1; this._currentIndex++)
+    for ( ; this._currentIndex < this._data.length-1, thisDate === nextDate; ++this._currentIndex)
     {
-        var d = this._data [this._currentIndex];
-       //todo...
+        var fixture = this._data [this._currentIndex];
+        var odds = fixture.odds;
+
+        if(odds > this._config.minOdds && odds < this._config.maxOdds )
+        {
+            if(!bestBet || bestBet.odds > odds)
+            {
+                bestBet = fixture;
+            } 
+        }
+        
+        if(this._data [this._currentIndex + 1])
+        {
+            nextDate = this._data [this._currentIndex + 1].date;
+        }
+    }
+
+    if (bestBet)
+    {
+        console.log ("best bet is ", bestBet.date, bestBet.odds);
+        if (bestBet.result === "D") {
+            this._balance += bestBet.odds -1;
+            console.log ("WIN!\t\t" + this._balance);
+        }
+        else{
+            this._balance--;
+            console.log ("LOSE!\t\t" + this._balance);
+        }
+    }
+
+    if(this._currentIndex < this._data.length-1)
+    {
+        placeNextBet.call (this);
     }
 }
 
@@ -27,23 +62,31 @@ function Emulator (config, data)
 {
     this._config = config;
 
-    //TODO - Need to ensure data is sorted chronologically
-    //this._data = data.sort (sortByDate);
 
     this._data = data;
 
-    this._balance = config.startinBalance;
+    //change dates from dd/mm/yy to yy/mm/dd ofr easier sorting
+    this._data = this._data.map (item => {
+        var ret = item;
+        var date = item.date.split ("/");
+        ret.date = date[2] + "/" + date[1] + "/" + date[0];
+        return ret;
+    })
+
+    this._data = data.sort (sortByDate);
+
+    this._balance = config.startingBalance;
     this._currentDate = data[0].date;
     this._currentIndex = 0;
 
     placeNextBet.call (this);
 
-    for (var i=0; i<data.length-1; i++)
+    for (var i=0; i<5; i++)
     {
         var d = data[i];
         for (var k in d)
         {
-            console.log ("d["+k+"] = " + d[k])
+            //console.log ("d["+k+"] = " + d[k])
         }
     }
 }
